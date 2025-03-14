636f2a18b71291203850e86e5580eb96e6c6b54d
ranfdev <ranfdev@gmail.com>
2025-03-14 20:38:50 +0100

    feat: CartItem to manage removedItems, special user requests. Add proper Cart implementation

--- src/models/Reservation.mjs
+++ src/models/Reservation.mjs
@@ -1,11 +1,49 @@
 import dayjs from 'dayjs';
+import Bag from './Bag.mjs';
 
 class Reservation {
-    constructor(id, userId, estId, createdAt) {
+    static STATUS_PENDING = 'pending';
+    static STATUS_CONFIRMED = 'confirmed';
+    static STATUS_CANCELED = 'canceled';
+
+    constructor(id, user, cartItems) {
         this.id = id;
-        this.userId = userId;
-        this.estId = estId;
-        this.createdAt = dayjs(createdAt).format('YYYY-MM-DD HH:mm:ss');
+        this.user = user;
+        this.createdAt = dayjs().toDate();
+        this.cartItems = cartItems;
+        this.status = Reservation.STATUS_PENDING;
+    }
+
+    validateCartItems(items) {
+        // Check pickup times are in future
+        items.forEach(item => {
+            if (item.bag.pickupStartItem <= this.createdAt) {
+                throw new Error('Cannot reserve expired bags');
+            }
+        });
+        return items;
+    }
+
+    confirm() {
+        if (this.cartItems.every(item => !item.bag.isReserved())) {
+            this.cartItems.forEach(item => {
+                item.bag.setReservedBy(this.user.id); // Track reserving user
+            });
+            this.status = Reservation.STATUS_CONFIRMED;
+            return true;
+        }
+        this.status = Reservation.STATUS_CANCELED;
+        return false;
+    }
+
+    cancel() {
+        const now = dayjs();
+        this.status = Reservation.STATUS_CANCELED;
+        this.cartItems.forEach(item => {
+            if (item.bag.pickupStart > now) {
+                item.bag.setReservedBy(null);
+            }
+        });
     }
 }
 
