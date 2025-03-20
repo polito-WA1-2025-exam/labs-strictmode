57be755bf9cb68eeb2477af41acc09ac5e7e17a8
ranfdev <ranfdev@gmail.com>
2025-03-21 00:08:28 +0100

    add getUserById

--- src/repos/UserRepo.mjs
+++ src/repos/UserRepo.mjs
@@ -1,4 +1,21 @@
 class UserRepo {
+    /**
+     * @param {string} email
+     * @param {string} assignedName
+     * @param {string} familyName
+     * @returns {User}
+     */
     createUser(email, assignedName, familyName) { }
+    /**
+     * @param {number} id
+     * @param {string} email
+     * @param {string} assignedName
+     * @param {string} familyName
+     */
     updateUser(id, email, assignedName, familyName) { }
+    /**
+     * @param {number} id
+     * @returns {User}
+     */
+    getUserById(id) { }
 }
