a90603d1e23100f483c37388f188613d1562d984
RiccardoRevalor <riccardorevalor@gmail.com>
2025-03-25 09:17:47 +0100

    feat: started cart mgmt endpoints + testing

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -20,6 +20,21 @@ const server = express();
 // TESTING
 /* SAMPLE DATA FOR TESTING */
 
+//global sample data
+//global available bags
+//global sample id for bags: 10, 20, 30, 40, 50
+const availableBags = [
+
+    new Bag(10, Bag.TYPE_REGULAR, 101, "large", "vegan",
+        5, "2025-03-30 10:00:00", "2025-04-31 12:00:00"),
+    new Bag(20, Bag.TYPE_REGULAR, 102, "large", "vegan",
+        10, "2025-03-13 14:00:00", "2025-07-14 16:00:00"),
+    new Bag(30, Bag.TYPE_REGULAR, 103, "large", "vegan",
+        15, "2025-03-20 14:00:00", "2025-09-21 16:00:00"),
+    new Bag(40, Bag.TYPE_REGULAR, 104, "large", "vegan",
+        20, "2025-03-25 14:00:00", "2025-03-24 16:00:00"),
+];
+
 const UserRepo_Testing = {
 
     users: [],
@@ -114,8 +129,11 @@ const CartRepo_Testing = {
     //add bag id
     currentBagId: 1,
 
+    //dictionary of carts, the key is the user id, the value is the cart
+    carts: {},
 
-    async getCart(userId) {
+
+    async getCart_sample(userId) {
         //return a new object Cart with the user's cart
         let sampleCart = new Cart(userId);
         //add the 2 bags
@@ -145,7 +163,65 @@ const CartRepo_Testing = {
         sampleCart.addItem(new CartItem(bag2));
 
         return sampleCart;
-    }
+    },
+
+    //get the cart of the user
+    async getCart(userId) {
+        //check if the user has a cart
+        if (!this.carts[userId]) {
+            return `Error: User ${userId} has no cart!`;
+        } else {
+            return this.carts[userId];
+        }
+    },
+
+
+    //add bag, so a cartItem,  to the user's cart
+    async addBag(userId, bagId) {
+        //check if the user already has a cart
+        //if not, create a new cart
+        //if yes, add the cartItem to the already existing cart
+        if (!this.carts[userId]) {
+            this.carts[userId] = new Cart(userId);
+        } else {
+            //check if the bag is already in the cart
+            //cartItem -> bag -> retrieve bagId
+            if (this.carts[userId].items.find(item => item.bag.id === bagId)) {
+                return `Error: Bag ${bagId} already in the cart!`;
+            } else {
+                //add the bag, so the cartItem, to the cart
+                const bag = availableBags.find(b => b.id === bagId);
+                if (!bag) {
+                    return `Error: Bag ${bagId} not found!`;
+                }
+                //add the corresponding cartItem, which can be further customized by the end user, to the cart
+                //Reminder: a cartItem is essentially a bag with a list of removed items, that can be choosen by the end user
+                this.carts[userId].addItem(new CartItem(bag));
+                return `Bag ${bagId} succesfully added to the cart!`;
+            }
+        }
+    },
+
+    //remove bag, so a cartItem, from the user's cart
+    async removeBag(userId, bagId) {
+        //check if the user has a cart
+        if (!this.carts[userId]) {
+            return `Error: User ${userId} has no cart!`;
+        } else {
+            //check if the bag, so the cartItem, is in the cart
+            //cartItem -> bag -> retrieve bagId
+            const index = this.carts[userId].items.findIndex(item => item.bag.id === bagId);
+            if (index === -1) {
+                return `Error: Bag ${bagId} not found in the cart!`;
+            } else {
+                //remove the bag from the cart
+                this.carts[userId].items.splice(index, 1);
+                return `Bag ${bagId} succesfully removed from the cart!`;
+            }
+        }
+
+    },
+
 
 
 }
@@ -285,7 +361,36 @@ async function deleteUser_Test(userRepo){
     });
 }
 
+/* CART ENDPOINTS */
 
+//get cart by userid
+async function getCart_Test(cartRepo){
+    server.get("/carts/:userId", async (req, res) => {
+        const userId = parseInt(req.params.userId);
+        const cart = await cartRepo.getCart(userId);
+        return res.json(cart);
+    });
+}
+
+//add bag, so a cartItem, to cart by userid
+async function addBag_Test(cartRepo){
+    server.post("/carts/:userId/bags", async (req, res) => {
+        const userId = parseInt(req.params.userId);
+        const { bagId } = req.body;
+        const result = await cartRepo.addBag(userId, parseInt(bagId));
+        return res.json(result);
+    });
+}
+
+//remove bag, so a cartItem, from cart by userid
+async function removeBag_Test(cartRepo){
+    server.delete("/carts/:userId/bags/:bagId", async (req, res) => {
+        const userId = parseInt(req.params.userId);
+        const bagId = parseInt(req.params.bagId);
+        const result = await cartRepo.removeBag(userId, bagId);
+        return res.json(result);
+    });
+}   
 
 
 /* RESERVATION ENDPOINTS */
@@ -322,7 +427,17 @@ async function createReservation_Test(userRepo, cartRepo, resRepo){
         }
         */
 
+        //for working with a sample pre set cart, use:
+        //const userCart = await cartRepo.getCart_Sample(userId);  //Retrieve user's cart
+        //otherwise, use:
         const userCart = await cartRepo.getCart(userId);  //Retrieve user's cart 
+
+        //check if the user has a cart, if not there's no sense in doing a reservation
+        if (userCart.startsWith("Error")){
+            return res.status(400).json("Error: User has no cart!");
+        }
+
+
         //User's cart contains all the bags
         //iterate over all the bags and:
         //1. do all the checks
@@ -416,6 +531,11 @@ createUser_Test(UserRepo_Testing);
 updateUser_Test(UserRepo_Testing);
 getUserById_Test(UserRepo_Testing);
 deleteUser_Test(UserRepo_Testing);
+
+//For Cart
+getCart_Test(CartRepo_Testing);
+addBag_Test(CartRepo_Testing);
+removeBag_Test(CartRepo_Testing);
 
 
 //For Reservation
